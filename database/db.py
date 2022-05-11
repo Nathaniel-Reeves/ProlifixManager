fc20c153bd6f00fbdedefd8c149c95b5e711457d
Nathaniel-Reeves <nathaniel.jacob.reeves@gmail.com>
2022-05-10 18:20:25 -0600

    started work on database class.  Figured out a format for documenting and writing code.  Only table and database connection functionality is created, table and data interaction still needed

--- database/db.py
+++ database/db.py
@@ -3,7 +3,14 @@ from mysql.connector import Error
 
 class database:
 
-    def __init__(self, database, table):
+    '''
+    database(database, table)
+    arg:
+    database (req) = name of database to connect to
+    table (optional) = name of table to connect to
+    Initializes a connection to a database.
+    '''
+    def __init__(self, database, table=None):
         self.connection = None
         self.database = database
         self.table = table
@@ -21,7 +28,94 @@ class database:
         except Error as e:
             print("Error while connecting to MySQL", e)
 
-    def createLineItem(self, name, date):
+    '''
+    obj.switchTable(table_name)
+    arg:
+    table_name (req) = name of table to switch to
+    '''
+    def switchTable(self, table):
+        self.table = table
+
+    '''
+    obj.getCurrentTable()
+    returns current table's name
+    '''
+    def getCurrentTable(self):
+        return self.table
+
+    '''
+    obj.getTables()
+    returns a list of all tables in the database
+    '''
+    def getTables(self):
+        sql_query = "SHOW tables;"
+        self.cursor.execute(sql_query)
+        tables = self.cursor.fetchall()
+        return tables[0]
+
+    '''
+    obj.tableExists(table_name):
+    arg:
+    table_name (opt)
+    returns:
+        boolian
+    '''
+    def tableExists(self, test=None):
+        if test is None:
+            test = self.table
+        if test is None:
+            return False
+        if test not in self.getTables():
+            return False
+        return True
+
+    '''
+    obj.getColumns(table_name, options)
+    arg:
+    table_name (opt) = name of a table in the database
+    options (opt)
+     -a, -all = return all column info in a list of tuples
+    returns (default):
+        column feild names in a list or None if table_name
+        doesn't exist
+    '''
+    def getColumns(self, *pram):
+        # valid option list
+        options = ['a','all']
+        # check and get table_name if any
+        if len(pram) == 0:
+            table = self.table
+        else:
+            if pram[0] in options:
+                table = self.table
+            else:
+                table = pram[0]
+        # get column data
+        if self.tableExists(table):
+            sql_query_temp = "SHOW COLUMNS FROM {};"
+            sql_query = sql_query_temp.format(table)
+            self.cursor.execute(sql_query)
+            columns = self.cursor.fetchall()
+        else:
+            return None
+        # return all column info
+        if len(pram) >= 1:
+            if "all" in pram or "a" in pram:
+                return columns
+        # return column field info only
+        fields = []
+        for column in columns:
+            fields.append(column[0])
+        return fields
+
+
+    def getAllItems(self):
+        sql_query = '''SELECT * FROM {}'''
+        sql_query_formatted = sql_query.format(self.table)
+
+        self.cursor.execute(sql_query_formatted)
+
+    def insertItem(self, name, date):
 
         sql_query = '''INSERT INTO {}(name, date) VALUES(%s, %s);'''
         sql_query_formated = sql_query.format(self.table)
@@ -30,6 +124,10 @@ class database:
         self.connection.commit()
         return
 
+    '''
+    del Operator Over-ride
+    close connections to database
+    '''
     def __del__(self):
         if (self.connection is not None and self.connection.is_connected()):
             self.cursor.close()
@@ -39,8 +137,7 @@ class database:
 names = ['joe','bill','tom','chad','henry','rick']
 dates = ['2022-01-01','2022-02-02','2022-03-03','2022-06-06','2022-04-04','2022-05-05',]
 
-test_table = database("test_database","test_table")
-for i in range(6):
-    test_table.createLineItem(names[i],dates[i])
-
-del test_table
+new_db = database("test_database")
+print(new_db.getColumns("test_table"))
+print(new_db.getColumns("test_table","all"))
+del new_db
