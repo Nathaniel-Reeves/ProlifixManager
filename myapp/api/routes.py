db8e1a958942bcdbd6a5618a35d584978835e0a8
Nathaniel J Reeves <80066234+Nathaniel-Reeves@users.noreply.github.com>
2022-09-16 09:56:08 -0600

    Delete Old test files

--- myapp/api/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-api = Blueprint('api', __name__, url_prefix='/api')
-
-@api.route('/getdata')
-def getdata():
-    return {'key':'value','key2':'value2', 'key3':'value3'}
--- myapp/site/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-site = Blueprint('site', __name__)
-
-@site.route('/')
-def index():
-    return '<h1>Welcome to the home page!</h1>'
