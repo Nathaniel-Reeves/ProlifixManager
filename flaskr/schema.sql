435ccbbfb64a9d9ceaae2f55d8d1cf3c4c3542b5
Nathaniel J Reeves <80066234+Nathaniel-Reeves@users.noreply.github.com>
2023-01-20 09:08:15 -0700

    Update schema.sql

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -25,7 +25,7 @@ CREATE TABLE `Organizations`.`User` (
   `username` VARCHAR(100) UNIQUE NOT NULL,
   `encrypted_password` VARCHAR(250) NOT NULL,
   `_json_schema` json GENERATED ALWAYS AS (_utf8mb4'{"type":"object"}') VIRTUAL,
-  `doc` json DEFAULT (CONCAT('{"_id":"',`user_id`,'","access_privileges":[]}')),
+  `doc` json DEFAULT (CONCAT('{"_id":',`user_id`,',"access_privileges":{"human_resources":"staff", "client_relations":"staff", "supplier_relations":"staff", "production":"staff", "logistics":"staff"}}')),
   PRIMARY KEY (`user_id`),
   CONSTRAINT `Org_User_t1_chk_1` CHECK (json_schema_valid(`_json_schema`,`doc`)) /*!80016 NOT ENFORCED */
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -334,11 +334,12 @@ INSERT INTO `Organizations`.`People` (`organization_id`, `first_name`, `last_nam
 
 -- Insert Nathaniel Reeves User Info  (Password = testpassword)
 INSERT INTO `Organizations`.`User` (`user_id`, `person_id`, `username`, `encrypted_password`) VALUES (1, 1, "nreeves", "pbkdf2:sha256:260000$xwmRNkYGEsbVxWQk$598deee9e52133d7d3a96eeb060c81f90b06d3ea17fb705b1e834855f5234df6");
+UPDATE `Organizations`.`User` SET `doc` = JSON_SET(`doc`, '$.access_privileges', '{human_resources:manager, client_relations:manager, supplier_relations:manager, production:manager, logistics:manager}') WHERE `user_id` = 1;
 
 -- Insert Kathy Jensen Person Info
-INSERT INTO `Organizations`.`People` (`organization_id`, `first_name`, `last_name`, `Job_Title`, `phone_number`, `email_address`, `is_employee`, `hourly_wage`, `contract_date`) VALUES 
+INSERT INTO `Organizations`.`People` (`organization_id`, `first_name`, `last_name`, `job_title`, `phone_number`, `email_address`, `is_employee`, `hourly_wage`, `contract_date`) VALUES 
 	(1, "Kathy", "Jensen", "Owner", "8016025244 ", "Info@holisticlifesupplements.com", true, 0, '2016-1-1');
 
 -- Insert Kathy Jensen User Info  (Password = password)
 INSERT INTO `Organizations`.`User` (`user_id`, `person_id`, `username`, `encrypted_password`) VALUES (2, 2, "kathyj", "pbkdf2:sha256:260000$D8qPhRKS15pNXdWb$7bd4d1a2603d4365d0711b7342a1a59f67fad6354b8424e574d0654cf276ec5c");
-UPDATE `Organizations`.`User` () 
+UPDATE `Organizations`.`User` SET `doc` = JSON_SET(`doc`, '$.access_privileges', '{human_resources:manager, client_relations:manager, supplier_relations:manager, production:manager, logistics:manager}') WHERE `user_id` = 2;
