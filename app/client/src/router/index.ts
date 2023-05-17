7c5b599a9c1b20c4f1c078b4ec7ea54a1982c207
Nathaniel J Reeves <80066234+Nathaniel-Reeves@users.noreply.github.com>
2023-05-17 16:09:26 -0600

    Started Dockerizing vue client

--- app/client/src/router/index.ts
+++ app/client/src/router/index.ts
@@ -1,46 +1,29 @@
 import Vue from 'vue';
-// import VueRouter, { RouteConfig } from 'vue-router';
-// import Home from '../views/Home.vue';
-import Router from 'vue-router';
+import VueRouter, { RouteConfig } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
 
-import Ping from '../components/Ping.vue';
+Vue.use(VueRouter);
 
-Vue.use(Router);
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
+  },
+];
 
-// Vue.use(VueRouter);
-//
-// const routes: Array<RouteConfig> = [
-//   {
-//     path: '/',
-//     name: 'Home',
-//     component: Home,
-//   },
-//   {
-//     path: '/about',
-//     name: 'About',
-//     // route level code-splitting
-//     // this generates a separate chunk (about.[hash].js) for this route
-//     // which is lazy-loaded when the route is visited.
-//     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-//   },
-// ];
-//
-// const router = new VueRouter({
-//   mode: 'history',
-//   base: process.env.BASE_URL,
-//   routes,
-// });
-//
-// export default router;
-
-export default new Router({
+const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/ping',
-      name: 'Ping',
-      component: Ping,
-    },
-  ],
+  routes,
 });
+
+export default router;
