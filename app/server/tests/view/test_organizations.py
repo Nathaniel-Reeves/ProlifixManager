687f3bb8130f97fcf98c900d89ba87a72c4a37fa
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2023-12-07 19:03:33 -0700

    attempting to mock authentication to test organization endpoints

--- app/server/tests/view/test_organizations.py
+++ app/server/tests/view/test_organizations.py
@@ -1,6 +1,5 @@
 import pytest
 
-from server import model as db
 from server.controler import organizations as org
 from server.wsgi import create_app
 from server import view
@@ -13,11 +12,44 @@ def describe_Organizations_Enpoints():
         
         @pytest.fixture
         def mock_controler_get_organizations(mocker):
-            return mocker.patch.object(org.get_organizations, return_value=view.CustomResponse(status_code=200))
+            responce = view.CustomResponse()
+            responce.set_status_code(200)
+            return mocker.patch(
+                    "org.get_organizations()", 
+                    return_value=responce
+                )
         
+        # @pytest.fixture
+        # def when_logged_in(mocker):
+        #     mocker.patch.object(
+        #             "Redis", 
+        #             return_value="fake_connection"
+        #         )
+        #     mocker.patch.object(
+        #             "redis_connection.ping()",
+        #             return_value=True
+        #         )
+        #     mocker.patch.object(
+        #             "request.cookies.get()",
+        #             return_value=True
+        #         )
+        #     mocker.patch.object(
+        #             "session_token",
+        #             return_value=True
+        #         )
+        #     mocker.patch.object(
+        #             "redis_connection.exists()",
+        #             return_value=True
+        #         )
+        #     mocker.patch.object(
+        #             "create_session()",
+        #             return_value="fake_session"
+        #         )
+
         def describe_api_orgnaizations():
             
-            def it_exists():
-                response = app.test_client().get('/api/organizations')
+            def it_exists(mocker, mock_controler_get_organizations):
+                url = '/api/organizations'
+                response = app.test_client().get(url)
                 
-                assert response.status_code == 308
+                assert response.status_code == 200
