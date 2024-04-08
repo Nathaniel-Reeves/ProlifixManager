080d17bd95d675f08c0dbe5e6063eabad4c53a05
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2024-04-08 17:37:58 -0600

    second attempt at fixing root and client user passwords in docker compose

--- app/db/privileges.sql
+++
@@ -1,6 +0,0 @@
-CREATE USER 'client'@'%' IDENTIFIED BY 'ClientPassword!5';
-GRANT ALL PRIVILEGES ON *.* TO 'client'@'%';
-FLUSH PRIVILEGES;
--- CREATE USER 'dallasj'@'%' IDENTIFIED BY 'dallasjPassword!5';
--- GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'dallasj'@'%';
--- FLUSH PRIVILEGES;
