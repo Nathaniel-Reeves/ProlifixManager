25c1f524ebfac45588a4be7c0c38ab227169c730
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2024-04-22 13:55:26 -0600

    Incremented minor release tag

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -85,7 +85,7 @@
       </div> -->
 
         <div class="text-center bg-dark p-3">
-          © 2024 Copyright  |  v1.1.1
+          © 2024 Copyright  |  v1.2.1
         </div>
       </footer>
     </div>
