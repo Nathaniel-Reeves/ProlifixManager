de59d8fdb623188bfb619f8792068dca3ab2ca9e
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2024-06-17 16:34:00 -0600

    Additinal bug fixing on the component specs template

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -101,7 +101,7 @@
       </div> -->
 
         <div class="text-center bg-dark p-3">
-          © 2024 Copyright | v2.0.3
+          © 2024 Copyright | v2.0.4
         </div>
       </footer>
     </div>
