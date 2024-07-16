1bac296c344f81f8d1490cace48ad9c7e3e64131
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2024-07-16 12:31:41 -0600

    update tag

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -101,7 +101,7 @@
       </div> -->
 
         <div class="text-center bg-dark p-3">
-          © 2024 Copyright | v3.0.2
+          © 2024 Copyright | v3.1.2
         </div>
       </footer>
     </div>
