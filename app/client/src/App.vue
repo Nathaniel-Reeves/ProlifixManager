a6e115a28317fed2d91a2939711c3b4aec14f6fe
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2024-07-17 15:14:28 -0600

    updated release code

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -101,7 +101,7 @@
       </div> -->
 
         <div class="text-center bg-dark p-3">
-          © 2024 Copyright | v3.1.2
+          © 2024 Copyright | v3.2.0
         </div>
       </footer>
     </div>
