eb2006c2be7754110babc453ae97d9ffedceaa49
Nathaniel Reeves <nathaniel.jacob.reeves@gmail.com>
2024-08-19 13:52:28 -0600

    bug fixing configure mariadb script

--- app/db/configure_mariadb.sh
+++ app/db/configure_mariadb.sh
@@ -55,7 +55,7 @@ fi
 ls -l
 
 # Execute all sql files in the docker-entrypoint-initdb.d directory
-for f in `docker-entrypoint-initdb.d -l *.sql`; do
+for f in `/docker-entrypoint-initdb.d -l *.sql`; do
     echo "Executing $f..."
     mysql -u "root" -p"$MARIADB_ROOT_PASSWORD" --host 172.10.10.2 < $f
     echo "$f has been executed."
